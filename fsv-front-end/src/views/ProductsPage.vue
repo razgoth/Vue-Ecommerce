8a6fd94707351f24ce2160b55c0df24a96415273
Ramzi Abu-Zahra <62545300+razgoth@users.noreply.github.com>
2022-02-13 19:04:40 +0100

    First Commit

---
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="page-wrap">
+    <div class="grid-wrap">
+      <ProductsGrid : products="products" />
+    </div>
+</div>  
+</template>
+
+<script>
+import { products } from '../fake-data';
+import ProductsGrid from '../components/ProductsGrid.vue';
+export default {
+  name: "ProductsPage",
+  components: {
+    ProductsGrid
+  },
+  data() {
+    return {
+     products,
+};
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
+
+
